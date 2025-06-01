d81031ab8ba6d72ec86b13005deff73df59119f5
iamniaina34 <iammandrindraniaina34@gmail.com>
2025-06-01 22:20:44 +0300

    edit: lombok-ed on Student

--- GestionNote/src/main/java/gestion/pack/model/Student.java
+++ GestionNote/src/main/java/gestion/pack/model/Student.java
@@ -1,43 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package gestion.pack.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
- *
  * @author ideal
+ * @author iamniaina34
  */
+@Getter
+@Setter
+@NoArgsConstructor
 public class Student {
     private int num;
     private String nom;
     private double moyenne;
-
-    public Student() {}
-
-    // Getters
-    public int getNum() {
-        return num;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public double getMoyenne() {
-        return moyenne;
-    }
-    //Setters
-    public void setNum(int num) {
-        this.num = num;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public void setMoyenne(double moyenne) {
-        this.moyenne = moyenne;
-    }
 }
 
