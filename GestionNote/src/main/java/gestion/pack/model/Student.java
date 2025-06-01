5fae3f8cea96e94297590b6d8df22fa7b039b123
iamniaina34 <iammandrindraniaina34@gmail.com>
2025-06-02 00:55:43 +0300

    fix: checked if moyenne is out of bound

--- GestionNote/src/main/java/gestion/pack/model/Student.java
+++ GestionNote/src/main/java/gestion/pack/model/Student.java
@@ -1,8 +1,6 @@
 package gestion.pack.model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 /**
  * @author ideal
@@ -11,15 +9,28 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class Student {
-    private int num;
+    private String numEt;
     private String nom;
     private double moyenne;
+
+    public Student(String numEt, String nom, double moyenne) {
+        this.numEt = numEt;
+        this.nom = nom;
+        setMoyenne(moyenne);
+    }
 
     public String getObs() {
         return moyenne >= 10
                 ? "Admis"
                 : moyenne >= 5 ? "Redouble" : "Exclus";
+    }
+
+    public void setMoyenne(double moyenne) {
+        if (moyenne < 0 || moyenne > 20)
+            throw new RuntimeException("Moyenne can't be lower than 0 or higher than 20");
+        this.moyenne = moyenne;
     }
 }
 
